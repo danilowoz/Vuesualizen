6bd487395c3a368f0f86d9ddc24a39143bf9c164
Danilo Woznica <danilowoz@gmail.com>
2017-10-03 22:22:18 -0300

    Update styles

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,12 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+
+@import 'components/animations.css';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Nunito', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
